7ee4dd7b7e2b33e035af7fb7552c9f2f79cac564
Kevin Eddye <kevneddy@gmail.com>
2016-07-11 12:43:58 -0700

    Add cluess to guesses and play again option

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # python-numberGame
+# limit number of guesses
+# catch when the number is not an integer 
+# give clues if number is too low or high
+# let ppl play again
--- randomNum.py
+++ randomNum.py
@@ -1,12 +1,28 @@
 import random
 
-secret_num = random.randint(1,10)
-
-while True:
-	guess = int(input("Guess a number between 1 and 10:"))
-
-	if guess == secret_num:
-		print("You got it! My number was {}".format(secret_num))
-		break
-	else:
-		print("That's not my number!")
+def game():
+  secret_num = random.randint(1,10)
+  guesses = []
+  
+  while len(guesses) < 5:
+    try:
+      guess = int(input("Guess a number between 1 and 10:"))
+    except ValueError:
+      print("{} isn't a number!".format(guess))
+    else:
+      if guess == secret_num:
+        print("You got it! My number was {}".format(secret_num))
+        break
+      elif guess < secret_num:
+        print("My number is higher than {}".format(secret_num))
+      else: 
+        print("My number is lower than {}".format(secret_num))
+      guesses.append(guess)
+  else:
+    print("You didn't get it, my number was {}".format(secret_num))
+  play_again = input("Do you want to play again? Y/n ")
+  if play_again.lower() != 'n':
+     game()
+  else:
+     print("Bye!")
+game()
